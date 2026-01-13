c71cf28bd397fe58a66028113efddc5daa2154a6
touhoufan <touhoufan2024@gmail.com>
2026-01-13 23:48:07 +0800

    Fix path references in daily_update.sh for consistency

--- daily_update.sh
+++ daily_update.sh
@@ -1,33 +1,33 @@
 set -e
 set -x
 
-[ ! -d "/dolt/investment_data" ] && echo "initializing dolt repo" && cd /dolt && dolt clone chenditc/investment_data
-cd /dolt/investment_data
+# [ ! -d "~/dolt/investment_data" ] && echo "initializing dolt repo" && cd ~/dolt && dolt clone chenditc/investment_data
+cd ~/dolt/investment_data
 dolt fetch origin master
 dolt reset origin/master
 dolt checkout .
 
 echo "Updating index weight"
 startdate=$(dolt sql -q "select * from max_index_date" -r csv | tail -1)
-python3 /investment_data/tushare/dump_index_weight.py --start_date=$startdate
-for file in $(ls /investment_data/tushare/index_weight/); 
+python3 ~/investment_data/tushare/dump_index_weight.py --start_date=$startdate
+for file in $(ls ~/investment_data/tushare/index_weight/); 
 do  
-  dolt table import -u ts_index_weight /investment_data/tushare/index_weight/$file; 
+  dolt table import -u ts_index_weight ~/investment_data/tushare/index_weight/$file; 
 done
 
 echo "Updating index price"
-python3 /investment_data/tushare/dump_index_eod_price.py 
-for file in $(ls /investment_data/tushare/index/); 
+python3 ~/investment_data/tushare/dump_index_eod_price.py 
+for file in $(ls ~/investment_data/tushare/index/); 
 do   
-  dolt table import -u ts_a_stock_eod_price /investment_data/tushare/index/$file; 
+  dolt table import -u ts_a_stock_eod_price ~/investment_data/tushare/index/$file; 
 done
 
 echo "Updating stock price"
 dolt sql-server &
-sleep 5 && python3 /investment_data/tushare/update_a_stock_eod_price_to_latest.py
+sleep 5 && python3 ~/investment_data/tushare/update_a_stock_eod_price_to_latest.py
 killall dolt
 
-dolt sql --file /investment_data/tushare/regular_update.sql
+dolt sql --file ~/investment_data/tushare/regular_update.sql
 
 dolt add -A
 
